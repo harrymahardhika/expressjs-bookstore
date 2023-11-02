f9730b459631b1aea99ab174703b246affc5e565
Harry Mahardhika <harrymahardhika@gmail.com>
2023-11-02 09:48:08 +0700

    fixes
    - database migration error
    - database seeder error

--- database/migrations.js
+++ database/migrations.js
@@ -1,45 +1,51 @@
 const db = require('../app/database')
 
-db.query('DROP TABLE IF EXISTS books')
-db.query('DROP TABLE IF EXISTS categories')
-db.query('DROP TABLE IF EXISTS authors')
-db.query('DROP TABLE IF EXISTS publishers')
+const migrate = async () => {
+  const conn = await db.getConnection()
 
-db.query(
-  `CREATE TABLE IF NOT EXISTS categories (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL
-  )`
-)
+  conn.execute('DROP TABLE IF EXISTS books')
+  conn.execute('DROP TABLE IF EXISTS categories')
+  conn.execute('DROP TABLE IF EXISTS authors')
+  conn.execute('DROP TABLE IF EXISTS publishers')
 
-db.query(
-  `CREATE TABLE IF NOT EXISTS authors (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL
-  )`
-)
+  conn.execute(
+    `CREATE TABLE IF NOT EXISTS categories (
+      id INT PRIMARY KEY AUTO_INCREMENT,
+      name VARCHAR(255) NOT NULL
+    )`
+  )
 
-db.query(
-  `CREATE TABLE IF NOT EXISTS publishers (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL
-  )`
-)
+  conn.execute(
+    `CREATE TABLE IF NOT EXISTS authors (
+      id INT PRIMARY KEY AUTO_INCREMENT,
+      name VARCHAR(255) NOT NULL
+    )`
+  )
 
-// Create a table for books
-db.query(
-  `CREATE TABLE IF NOT EXISTS books (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(255) NOT NULL,
-    author_id INT,
-    category_id INT,
-    publisher_id INT,
-    published_year INT,
-    isbn VARCHAR(13),
-    FOREIGN KEY (author_id) REFERENCES authors(id),
-    FOREIGN KEY (category_id) REFERENCES categories(id),
-    FOREIGN KEY (publisher_id) REFERENCES publishers(id)
-  )`
-)
+  conn.execute(
+    `CREATE TABLE IF NOT EXISTS publishers (
+      id INT PRIMARY KEY AUTO_INCREMENT,
+      name VARCHAR(255) NOT NULL
+    )`
+  )
 
-db.end()
+  // Create a table for books
+  conn.execute(
+    `CREATE TABLE IF NOT EXISTS books (
+      id INT PRIMARY KEY AUTO_INCREMENT,
+      title VARCHAR(255) NOT NULL,
+      author_id INT,
+      category_id INT,
+      publisher_id INT,
+      published_year INT,
+      isbn VARCHAR(13),
+      FOREIGN KEY (author_id) REFERENCES authors(id),
+      FOREIGN KEY (category_id) REFERENCES categories(id),
+      FOREIGN KEY (publisher_id) REFERENCES publishers(id)
+    )`
+  )
+
+  conn.release()
+}
+
+migrate()
--- database/seeders.js
+++ database/seeders.js
@@ -1,16 +1,22 @@
 const { faker } = require('@faker-js/faker')
 const db = require('../app/database')
 
-for (let i = 0; i < 10; i++) {
-  db.query(`INSERT INTO categories (name) VALUES (?)`, [faker.lorem.word()])
+const seed = async () => {
+  const conn = await db.getConnection()
+
+  for (let i = 0; i < 10; i++) {
+    conn.query(`INSERT INTO categories (name) VALUES (?)`, [faker.lorem.word()])
+  }
+
+  for (let i = 0; i < 10; i++) {
+    conn.query(`INSERT INTO authors (name) VALUES (?)`, [faker.person.fullName()])
+  }
+
+  for (let i = 0; i < 10; i++) {
+    conn.query(`INSERT INTO publishers (name) VALUES (?)`, [faker.company.name()])
+  }
+
+  conn.release()
 }
 
-for (let i = 0; i < 10; i++) {
-  db.query(`INSERT INTO authors (name) VALUES (?)`, [faker.person.fullName()])
-}
-
-for (let i = 0; i < 10; i++) {
-  db.query(`INSERT INTO publishers (name) VALUES (?)`, [faker.company.name()])
-}
-
-db.end()
+seed()
